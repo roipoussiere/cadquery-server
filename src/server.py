d0b2202383a33f14285e3486ac610c955f55f97d
Roipoussiere <roipoussiere@protonmail.com>
2022-08-30 12:46:32 +0200

    add flask template poc

--- src/server.py
+++ src/server.py
@@ -1,25 +1,34 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 from .module_manager import CadQueryModuleManagerError
 
 
 app = Flask(__name__)
 
+
 def run(port, module_manager):
     module_manager.init()
 
     @app.route('/', methods = [ 'GET' ])
     def root():
+        return render_template(
+            'viewer.html',
+            module=request.args.get('module'),
+            object=request.args.get('object')
+        )
+
+    @app.route('/json', methods = [ 'GET' ])
+    def json():
         module_name = request.args.get('module')
         object_var_name = request.args.get('object')
-        format = request.args.get('format')
 
         try:
-            return module_manager.render(module_name, object_var_name, format)
+            return module_manager.render(module_name, object_var_name, 'json')
         except CadQueryModuleManagerError as err:
-            response = '<p>%s</p>' % err.message
-            if err.stacktrace:
-                response += '<pre>%s</pre>' % err.stacktrace
+            response = {
+                'msg': err.message,
+                'stacktrace': err.stacktrace
+            }
             return response, 400
 
     app.run(host='0.0.0.0', port=port, debug=False)
