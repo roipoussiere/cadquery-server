34485a826feae6bf5b5df53c0bdbef4b274c4977
Roipoussiere <roipoussiere@protonmail.com>
2022-08-30 16:32:10 +0200

    display model as html instead json, using three-cad-viewer

--- src/static/viewer.js
+++ src/static/viewer.js
@@ -1,10 +1,27 @@
-function updateJson(module_name, object_var) {
+import { Viewer } from './vendor/three-cad-viewer.esm.js';
+
+const container = document.getElementById('cad_view');
+const options = {
+	cadWidth: window.innerWidth - 8,
+	height: window.innerHeight - 44,
+	treeWidth: window.innerWidth > 400 ? window.innerWidth / 3 : 200,
+	glass: true
+};
+const viewer = new Viewer(container, options, () => {});
+viewer.trimUI(["axes", "axes0", "grid", "ortho", "more", "help"], false);
+
+function render(model) {
+	viewer.clear();
+	const [ shapes, states ] = model;
+	const [ group, tree ] = viewer.renderTessellatedShapes(shapes, states, options);
+	viewer.render(group, tree, states, options);
+}
+
+function update_model(module_name, object_var) {
 	fetch(`json?module=${ module_name }&object=${ object_var }`)
 		.then(response => response.json())
-		.then(json => {
-			document.getElementById('json').innerHTML = JSON.stringify(json, null, 4);
-		})
-		.catch(error => {
-			console.log(error);
-		});
+		.then(model => render(model))
+		.catch(error => console.log(error));
 }
+
+export { update_model };
