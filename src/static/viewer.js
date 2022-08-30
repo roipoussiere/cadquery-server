8581edba0422de04d8aa7245ae608019cc87cc30
Roipoussiere <roipoussiere@protonmail.com>
2022-08-30 17:06:13 +0200

    update view when resizing window

--- src/static/viewer.js
+++ src/static/viewer.js
@@ -1,20 +1,27 @@
 import { Viewer } from './vendor/three-cad-viewer.esm.js';
 
 const container = document.getElementById('cad_view');
-const options = {
-	cadWidth: window.innerWidth - 8,
-	height: window.innerHeight - 44,
-	treeWidth: window.innerWidth > 400 ? window.innerWidth / 3 : 200,
-	glass: true
-};
-const viewer = new Viewer(container, options, () => {});
-viewer.trimUI(["axes", "axes0", "grid", "ortho", "more", "help"], false);
+let viewer = build_viewer();
+let model = [ {}, {} ];
 
-function render(model) {
+function build_viewer() {
+	const options = {
+		cadWidth: window.innerWidth - 8,
+		height: window.innerHeight - 44,
+		treeWidth: window.innerWidth > 400 ? window.innerWidth / 3 : 200,
+		glass: true
+	};
+	const viewer = new Viewer(container, options, () => {});
+	viewer.trimUI(["axes", "axes0", "grid", "ortho", "more", "help"], false);
+	return viewer;
+}
+
+function render(_model) {
+	model = _model
 	viewer.clear();
 	const [ shapes, states ] = model;
-	const [ group, tree ] = viewer.renderTessellatedShapes(shapes, states, options);
-	viewer.render(group, tree, states, options);
+	const [ group, tree ] = viewer.renderTessellatedShapes(shapes, states, {});
+	viewer.render(group, tree, states, {});
 }
 
 function update_model(module_name, object_var) {
@@ -23,5 +30,10 @@ function update_model(module_name, object_var) {
 		.then(model => render(model))
 		.catch(error => console.log(error));
 }
+
+window.addEventListener('resize', event => {
+	viewer = build_viewer();
+	render(model);
+});
 
 export { update_model };
