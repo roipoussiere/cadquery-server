632954abaa30acc164695683d745260f0394dda6
jdegenstein <jdegenstein@users.noreply.github.com>
2022-09-12 10:55:04 -0500

    Improve style of example
    Remove line-break slashes and replace with parentheses per psf/black formatter guidelines.

--- examples/cq_tutorial.py
+++ examples/cq_tutorial.py
@@ -7,11 +7,11 @@ thickness = 10.0
 diameter = 22.0
 padding = 12.0
 
-model = cq.Workplane("XY").box(height, width, thickness) \
-    .faces(">Z").workplane().hole(diameter) \
-    .faces(">Z").workplane() \
-    .rect(height - padding, width - padding, forConstruction=True) \
-    .vertices().cboreHole(2.4, 4.4, 2.1) \
+model = (cq.Workplane("XY").box(height, width, thickness)
+    .faces(">Z").workplane().hole(diameter)
+    .faces(">Z").workplane()
+    .rect(height - padding, width - padding, forConstruction=True)
+    .vertices().cboreHole(2.4, 4.4, 2.1)
     .edges("|Z").fillet(2.0)
-
+)
 show_object(model)
