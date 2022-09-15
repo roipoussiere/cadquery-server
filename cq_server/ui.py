0f87fac0f261405f1b5defe1019b586a0dc694c2
Roipoussiere <roipoussiere@protonmail.com>
2022-09-15 18:36:57 +0200

    ui.py: work with cq object (Assembly and Color)

--- cq_server/ui.py
+++ cq_server/ui.py
@@ -1,12 +1,11 @@
 '''Module ui: define UI class and render functions (show_object, …). Used by the CadQuery script.'''
 
 import json
-from typing import List
-from jupyter_cadquery.utils import numpy_to_json, Color
+from cadquery import Color, Assembly
 
 
-MODEL_OPTIONS_DEFAULT   = { 'color': (232, 176, 36), 'alpha': 1   }
-MODEL_OPTIONS_DEBUG = { 'color': (255,   0,  0), 'alpha': 0.2 }
+MODEL_COLOR_DEFAULT = Color(0.91, 0.69, 0.14)
+MODEL_COLOR_DEBUG   = Color(1, 0, 0, 0.2)
 
 
 class UI: # pylint: disable=too-few-public-methods
@@ -14,26 +13,21 @@ class UI: # pylint: disable=too-few-public-methods
     Must be imported by the CadQuery script.'''
 
     def __init__(self) -> None:
-        self.names: List[str] = []
-        self.models: list = []
-        self.colors: List[Color] = []
+        self.assembly = Assembly()
 
     # pylint: disable=import-outside-toplevel
     def get_model(self) -> list:
         '''Return the tesselated model of the object passed in the show_object() function,
         as a dictionnary usable by three-cad-viewer.'''
 
-        if not self.models:
+        if not self.assembly.children:
             return ''
 
         from jupyter_cadquery.cad_objects import to_assembly
         from jupyter_cadquery.base import _tessellate_group
+        from jupyter_cadquery.utils import numpy_to_json
 
-        assembly = to_assembly(*self.models, names=self.names)
-        for idx, obj in enumerate(assembly.objects):
-            if hasattr(obj, 'color'):
-                obj.color = self.colors[idx]
-
+        assembly = to_assembly(*self.assembly.children)
         assembly_tesselated = _tessellate_group(assembly)
         assembly_json = numpy_to_json(assembly_tesselated)
         assembly_dict = json.loads(assembly_json)
@@ -42,29 +36,34 @@ class UI: # pylint: disable=too-few-public-methods
         return assembly_dict
 
     def clear(self):
-        '''Clear all data stored in the UI instance.'''
+        '''Clear assembly.'''
 
-        self.names  = []
-        self.models = []
-        self.colors = []
+        self.assembly = Assembly()
 
 
 ui = UI()
 
 
-def show_object(*models, name: str=None, options: dict={}) -> None:
-    '''Store the given model to ui object in order to allow CadQuery Server to render it.'''
+def show_object(*models, name: str|None=None, options: dict={}) -> None:
+    '''Add the given model(s) to ui assembly in order to allow CadQuery Server to render it.'''
 
-    color = Color(options.get('color', MODEL_OPTIONS_DEFAULT['color']))
-    color.a = options.get('alpha', MODEL_OPTIONS_DEFAULT['alpha'])
+    rgb = options.get('color', None)
+    alpha = options.get('alpha', None)
 
-    for model in models:
-        ui.names.append(name)
-        ui.models.append(model)
-        ui.colors.append(color)
+    color = rgb          if type(rgb) == Color \
+        else Color(rgb)  if type(rgb) == str \
+        else Color(*rgb) if type(rgb) == tuple \
+        else MODEL_COLOR_DEFAULT
+
+    if alpha:
+        color = Color(color.toTuple()[:3] + [ alpha ])
+
+    for counter, model in enumerate(models):
+        _name = f'{ name }_{ counter }' if name and len(models) > 1 else name
+        ui.assembly.add(model, name=_name, color=color)
 
 
-def debug(model, name: str = None) -> None:
-    '''Same as show_object() but with predefined debug color and alpha used for debugging.'''
+def debug(model, name: str|None=None) -> None:
+    '''Same as show_object() but with the predefined debug color.'''
 
-    show_object(model, name=name, options = MODEL_OPTIONS_DEBUG)
+    show_object(model, name=name, options={ 'color': MODEL_COLOR_DEBUG })
