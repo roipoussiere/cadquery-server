de27ade5fe1c2e5e062b193b12ebad3ca3469a15
Roipoussiere <roipoussiere@protonmail.com>
2022-09-16 18:48:19 +0200

    improve png export

--- cq_server/exporter.py
+++ cq_server/exporter.py
@@ -17,77 +17,99 @@ from .module_manager import ModuleManager
 APP_DIR = op.dirname(__file__)
 STATIC_DIR = op.join(APP_DIR, 'static')
 TEMPLATES_DIR = op.join(APP_DIR, 'templates')
+DEFAULT_SVG_OPTIONS = {
+    'width': 50,
+    'height': 50,
+    'marginLeft': 5,
+    'marginTop': 5,
+    'showAxes': False,
+    'strokeColor': (50, 50, 50),
+    'hiddenColor': (100, 100, 100),
+    'showHidden': True,
+    'backgroundColor': '#aaa' # used for rasterization
+}
 
 
 class Exporter:
+    '''Class used to export the target in many formats.'''
+
     def __init__(self, module_manager: ModuleManager):
         self.module_manager = module_manager
         self.module_manager.init()
 
-    def _saving(self, destination: str, format: str, save: callable):
+    def _saving(self, destination: str, file_format: str, save: callable):
         if op.dirname(destination) and not op.isdir(op.dirname(destination)):
             os.makedirs(op.dirname(destination))
 
         save()
 
-        print(f'{ format } file exported in { destination }.')
+        print(f'{ file_format } file exported in { destination }.')
 
-    def _save_data_to(self, destination: str, format: str, data: str):
+    def _save_data_to(self, destination: str, data: str):
         if destination == '-':
             print(data)
         else:
             with open(destination, 'w', encoding='utf-8') as file:
                 file.write(data)
 
-    def save_to_html(self, destination: str, ui_options: dict={}, minify: bool=True):
-        def save():
-            html = self.get_html(ui_options, minify)
-            self._save_data_to(destination, 'html', html)
-
-        self._saving(destination, 'html', save)
-
-    def save_to(self, destination: str, format: str):
-        def save():
-            if format == 'json' :
-                self._save_data_to(destination, format, self.get_json())
-            elif format == 'js' :
-                self._save_data_to(destination, format, self.get_js())
-            else:
-                self.save(destination, format)
-
-        self._saving(destination, format, save)
-
-    def save(self, destination: str, format: str) -> str:
+    def _save(self, destination: str, file_format: str, options=None) -> str:
         '''Save the assembly in the given format.'''
 
         assembly = self.module_manager.get_assembly()
+        if not options and file_format in [ 'svg', 'png', 'pdf' ]:
+            options = DEFAULT_SVG_OPTIONS
 
-        if format in [ 'step', 'xml', 'gltf', 'vtkjs', 'vrml' ]:
-            assembly.save(destination, exportType=format.upper())
-        elif format in [ 'dxf', 'svg', 'stl', 'amf', 'tjs', 'vtp', '3mf' ]:
-            exporters.export(assembly.toCompound(), destination, format.upper())
-        elif format in [ 'png' , 'pdf' ]:
+        if file_format in [ 'step', 'xml', 'gltf', 'vtkjs', 'vrml' ]:
+            assembly.save(destination, exportType=file_format.upper())
+        elif file_format in [ 'dxf', 'svg', 'stl', 'amf', 'tjs', 'vtp', '3mf' ]:
+            exporters.export(assembly.toCompound(), destination, file_format.upper(), opt=options)
+        elif file_format in [ 'png' , 'pdf' ]:
             with tempfile.NamedTemporaryFile() as svg_file:
-                exporters.export(assembly.toCompound(), svg_file.name, 'SVG')
-                export = cairosvg.svg2png if format == 'svg' else cairosvg.svg2pdf
-                export(file_obj=svg_file, write_to=destination)
+                exporters.export(assembly.toCompound(), svg_file.name, 'SVG', opt=options)
+                if file_format == 'png':
+                    cairosvg.svg2png(file_obj=svg_file, write_to=destination, scale=2,
+                        background_color=options.get('backgroundColor', None))
+                else:
+                    cairosvg.svg2pdf(file_obj=svg_file, write_to=destination)
         else:
-            raise NameError(f'bad export format: { format }')
+            raise NameError(f'bad export format: { file_format }')
+
+    def save_to_html(self, destination: str, ui_options: dict, minify: bool=True):
+        '''Save a static html page that renders the assembly.'''
+
+        def save():
+            html = self.get_html(ui_options, minify)
+            self._save_data_to(destination, html)
+
+        self._saving(destination, 'html', save)
+
+    def save_to(self, destination: str, file_format: str):
+        '''Save the assembly in the given format, including json and js.'''
+
+        def save():
+            if file_format == 'json' :
+                self._save_data_to(destination, self.get_json())
+            elif file_format == 'js' :
+                self._save_data_to(destination, self.get_js())
+            else:
+                self._save(destination, file_format)
+
+        self._saving(destination, file_format, save)
 
     def get_json(self) -> str:
-        '''Return model data as json string.'''
-        
+        '''Return assembly data as json string.'''
+
         data = self.module_manager.get_data()
         return json.dumps(data)
 
     def get_js(self) -> str:
-        '''Return model data as js file containing the json.'''
+        '''Return assembly data as js file containing the json.'''
 
         json_data = self.get_json()
         return f"modules['{ self.module_manager.module_name }'] = { json_data }"
 
     def get_html(self, ui_options: dict, minify: bool=True) -> str:
-        '''Return the html string of a page that renders the target defined in the module manager.'''
+        '''Return the html string of a page that renders the assembly.'''
 
         viewer_css_path = op.join(STATIC_DIR, 'viewer.css')
         viewer_js_path = op.join(STATIC_DIR, 'viewer.js')
