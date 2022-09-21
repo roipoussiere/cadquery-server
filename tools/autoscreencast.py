5748353ec9c130350353b7184fb868f719753234
Roipoussiere <roipoussiere@protonmail.com>
2022-09-21 13:37:21 +0200

    improve screencast script

--- tools/autoscreencast.py
+++ tools/autoscreencast.py
@@ -21,14 +21,11 @@ import pyperclip
 class AutoScreencast:
     'Main class of the AutoScreencast tool.'
 
-    keys_interval = 0.1
-    text_interval = 0.5
-
-    def __init__(self, video_dir: str):
+    def __init__(self, video_dir: str, prefix: str):
         self.video_dir = op.abspath(video_dir)
 
-        ag.PAUSE = 0
-        file_name_prefix = f"demo_{ datetime.now().strftime('%Y-%m-%d_%H:%M:%S') }"
+        ag.PAUSE = 0.3
+        file_name_prefix = f"{ prefix }_{ datetime.now().strftime('%Y-%m-%d_%H:%M:%S') }"
         self.video_path_prefix = op.join(self.video_dir, file_name_prefix)
         self.proc = None
         self.subtitles = ''
@@ -38,19 +35,17 @@ class AutoScreencast:
     def type(self, text: str, slow=True):
         'type text, with a workaround to support special characters'
 
+        pause = ag.PAUSE
+        ag.PAUSE = 0
         for char in text:
             pyperclip.copy(char)
             ag.hotkey('ctrl', 'shift', 'v')
             if slow:
-                sleep(self.keys_interval * random())
-                if char == ' ':
-                    sleep(self.text_interval * random())
+                sleep((pause if char == ' ' else ag.MINIMUM_DURATION) * random())
 
-        sleep(self.keys_interval)
+        ag.PAUSE = pause
         ag.hotkey('enter')
-        sleep(self.keys_interval)
-        if slow:
-            sleep(self.text_interval)
+        sleep(pause)
 
     def screencast(self):
         'function to start screencast using ffmpeg, called in an other thread.'
@@ -74,12 +69,10 @@ class AutoScreencast:
         sleep(1)
 
     def end(self):
-        'exit the screencast properly'
+        'exit the screencast properly and write subtitles'
 
-        sleep(0.5)
+        sleep(1)
         self.proc.terminate()
-        sleep(0.5)
-        ag.hotkey('enter')
 
         with open(self.video_path_prefix + '.srt', 'w', encoding='utf-8') as sub_file:
             sub_file.write(self.subtitles)
@@ -88,12 +81,28 @@ class AutoScreencast:
         'move cursor'
 
         pos_x, pos_y = pos.replace(' ', '').split(',')
-        ag.moveTo(int(pos_x), int(pos_y), duration=1.5)
+        ag.moveTo(int(pos_x), int(pos_y), 1, ag.easeOutQuad)
+
+    def click(self, pos: str=''):
+        'mouse click'
+
+        if pos:
+            self.move(pos)
+        ag.click()
+
+    def drag(self, pos: str):
+        'click then move cursor'
+
+        pos_x, pos_y = pos.replace(' ', '').split(',')
+        ag.dragTo(int(pos_x), int(pos_y), 1, ag.easeOutQuad)
 
     def keys(self, keys: str):
         'hit keys'
 
-        ag.hotkey(*keys.split('+'))
+        for key in keys.replace(' ', '').split(','):
+            ag.hotkey(*key.split('+'))
+
+        sleep(ag.PAUSE)
 
     def subt(self, text: timedelta):
         'add a subtitle'
@@ -116,14 +125,24 @@ class AutoScreencast:
             try:
                 for line in md_file.readlines():
                     line = line.strip('\n')
-                    if line == '' or line.startswith('>'):
+                    if line == '' or line.startswith('<!--'):
                         pass
                     if line.startswith('    '):
                         self.type(line[4:])
-                    elif line.startswith('- `'):
-                        self.keys(line[3:-1])
-                    elif line.startswith('- *'):
-                        self.move(line[3:-1])
+                    elif line.startswith('__`'):
+                        self.keys(line[3:-3])
+                    elif line.startswith('---'):
+                        sleep(0.5 * (len(line) - 2))
+                    elif line.startswith('> move to '):
+                        self.move(line[10:])
+                    elif line.startswith('> click to '):
+                        self.click(line[11:])
+                    elif line.startswith('> click'):
+                        self.click()
+                    elif line.startswith('> drag to '):
+                        self.drag(line[10:])
+                    elif line.startswith('>'):
+                        pass
                     else:
                         self.subt(line)
             except Exception as error:
@@ -131,7 +150,7 @@ class AutoScreencast:
                 raise error
 
 
-sch = AutoScreencast('./screencasts')
+sch = AutoScreencast('./screencasts', 'demo')
 sch.init()
-sch.read('./screencast.md')
+sch.read('./tools/screencast.md')
 sch.end()
