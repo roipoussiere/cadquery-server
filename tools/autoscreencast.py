fe901972db2a9af2d91d26c3dcbef2f6af6d96a9
Roipoussiere <roipoussiere@protonmail.com>
2022-09-22 21:23:50 +0200

    update screencast script

--- tools/autoscreencast.py
+++ tools/autoscreencast.py
@@ -24,7 +24,7 @@ class AutoScreencast:
     def __init__(self, video_dir: str, prefix: str):
         self.video_dir = op.abspath(video_dir)
 
-        ag.PAUSE = 0.3
+        ag.PAUSE = 0.2
         file_name_prefix = f"{ prefix }_{ datetime.now().strftime('%Y-%m-%d_%H:%M:%S') }"
         self.video_path_prefix = op.join(self.video_dir, file_name_prefix)
         self.proc = None
@@ -41,7 +41,8 @@ class AutoScreencast:
             pyperclip.copy(char)
             ag.hotkey('ctrl', 'shift', 'v')
             if slow:
-                sleep((pause if char == ' ' else ag.MINIMUM_DURATION) * random())
+                base_delay = pause if char == ' ' else ag.MINIMUM_DURATION
+                sleep(base_delay * 0.25 + base_delay * 0.5 * random())
 
         ag.PAUSE = pause
         ag.hotkey('enter')
@@ -50,11 +51,22 @@ class AutoScreencast:
     def screencast(self):
         'function to start screencast using ffmpeg, called in an other thread.'
 
-        str_size = '%dx%d' % ag.size()
-        video_path = self.video_path_prefix + '.mp4'
-        comand = f'ffmpeg -loglevel error -f x11grab -s { str_size } -r 25 -i :0.0 { video_path }'
+        # pylint: disable=consider-using-f-string
+        command = ('ffmpeg '
+            + '-loglevel error '
+            + '-video_size %dx%d ' % ag.size()
+            + '-framerate 15 '
+            + '-f x11grab '
+            + '-i :0.0 '
+            + '-an '
+            + '-vcodec libx264 '
+            + '-crf 0 '
+            + '-preset ultrafast '
+            + '%s.mp4 ' % self.video_path_prefix
+            + '-thread_queue_size 1024'
+        )
 
-        self.proc = Popen(comand.split(' '))
+        self.proc = Popen(command.split(' '))
 
     def init(self):
         'init the screencast'
@@ -81,7 +93,7 @@ class AutoScreencast:
         'move cursor'
 
         pos_x, pos_y = pos.replace(' ', '').split(',')
-        ag.moveTo(int(pos_x), int(pos_y), 1, ag.easeOutQuad)
+        ag.moveTo(int(pos_x), int(pos_y), 0.75, ag.easeOutQuad)
 
     def click(self, pos: str=''):
         'mouse click'
@@ -111,7 +123,7 @@ class AutoScreencast:
             return str(delta)[:-3].replace('.', ',')
 
         start = timedelta(seconds=time()) - self.startup
-        end = start + timedelta(seconds=1)
+        end = start + timedelta(seconds=2)
 
         self.subtitles += f'{ str(self.sub_counter) }\n'
         self.subtitles += f'{ delta_to_str(start) } --> { delta_to_str(end) }\n'
@@ -126,7 +138,7 @@ class AutoScreencast:
                 for line in md_file.readlines():
                     line = line.strip('\n')
                     if line == '' or line.startswith('<!--'):
-                        pass
+                        continue
                     if line.startswith('    '):
                         self.type(line[4:])
                     elif line.startswith('__`'):
@@ -134,15 +146,15 @@ class AutoScreencast:
                     elif line.startswith('---'):
                         sleep(0.5 * (len(line) - 2))
                     elif line.startswith('> move to '):
-                        self.move(line[10:])
+                        self.move(line.split('(')[1].split(')')[0])
                     elif line.startswith('> click to '):
-                        self.click(line[11:])
+                        self.click(line.split('(')[1].split(')')[0])
                     elif line.startswith('> click'):
                         self.click()
                     elif line.startswith('> drag to '):
-                        self.drag(line[10:])
+                        self.drag(line.split('(')[1].split(')')[0])
                     elif line.startswith('>'):
-                        pass
+                        continue
                     else:
                         self.subt(line)
             except Exception as error:
